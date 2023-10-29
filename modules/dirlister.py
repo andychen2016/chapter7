dea5449faf7444206f2b39ed16364af1157ead2d
andychen <alexcxdq@163.com>
2023-10-29 12:57:44 +0800

    Update dirlister.py 适应python3

--- modules/dirlister.py
+++ modules/dirlister.py
@@ -3,7 +3,7 @@
 import os
 
 def run(**args):
-    print "[*] In dirlister module."
+    print（"[*] In dirlister module."）
     # 列出当前目录的所有文件,并作为字符串返回
     files = os.listdir(".")
     return str(files)
